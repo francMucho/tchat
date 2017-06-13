7c479313722711b400cdbb25ba734f7e1d0ef088
francMucho <franc.muchongabo@gmail.com>
2017-06-13 11:04:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>CoeurMundi</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+   
+    <link href="css/font.css" rel="stylesheet">
+
+  </head>
+  <body>
+<div class="container">
+          <div class="row" style="margin-top: 30px">
+         
+            <div class="col-lg-1"></div>
+
+
+
+
+                    <div class="col-lg-4 col-xs-18 col-sm-4 "> <!-- ms large et xs hoteur -->
+                 
+       
+                <h2><b>Login via</b></h2>
+                <div class="social-buttons">
+                  <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i><h4> Facebook </h4></a>
+                  <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> <h4> Twitter </h4></a>
+                </div>
+                                <b>or</b>
+                 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                    <div class="form-group">
+                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                    </div>
+                    <div class="form-group">
+                       <label class="sr-only" for="exampleInputPassword2">Password</label>
+                       <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                             <div class="help-block text-right"><a href="#"><h4>Forget the password ?</h4></a></div>
+                    </div>
+                    <div class="form-group">
+                       <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                    </div>
+                   <!--  <div class="checkbox">
+                       <label>
+                       <input type="checkbox"> <h4>keep me logged-in</h4>
+                       </label>
+                    </div> -->
+                 </form>
+                  <a href="registration.php" class="btn btn-fb"><i class="fa fa-facebook"></i> Create a cont </a>
+        
+             <!--  <div class="bottom text-center">
+              New here ? <a href="#"><b>Join Us</b></a>
+              </div> -->
+          
+           </div>
+           </div>
+           </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
